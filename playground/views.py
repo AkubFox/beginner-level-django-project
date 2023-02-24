5260364041469978389890e72cbff32eee0978c2
AkubFox <akubfox@gmail.com>
2023-02-24 18:21:19 +0100

    Testing Debuging

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+def calculate():
+    x = 1
+    y = 2
+    x += y
+    return x
+
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'Hello.html', {'name': 'Jacob'})
